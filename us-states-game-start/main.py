7fe49b49a46811386943f3d7733ef22daa3a6be6
fcmdhyd <fcmdhyd@gmail.com>
2022-03-10 23:43:14 +0800

    completed

--- us-states-game-start/main.py
+++ us-states-game-start/main.py
@@ -1,4 +1,5 @@
 import turtle
+import pandas
 
 screen = turtle.Screen()
 screen.title("U.S. State Game")
@@ -6,7 +7,31 @@ image = "blank_states_img.gif"
 screen.addshape(image)
 turtle.shape(image)
 
-answer_state = screen.textinput(title="Guess the State", prompt="Name a State:")
-print(answer_state)
+data = pandas.read_csv("50_states.csv")
+all_states = data.state.to_list()
+guessed_states = []
+
+while len(guessed_states) < 50:
+    answer_state = screen.textinput(title="Guess the State", prompt="Name a State:").title()
+        # If answer_state is one of the states in all the states of the 50_states.csv
+            # If they got it right:
+                # Create a turtle to write the name of the state at the state's x and y coordinate location
+    # State to learn
+    if answer_state == "Exit":
+        missing_states = []
+        for state in all_states:
+            if state not in guessed_states:
+                missing_states.append(state)
+        new_data = pandas.DataFrame(missing_states)
+        new_data.to_csv("states_to_learn")
+
+    if answer_state in all_states:
+        guessed_states.append((answer_state))
+        t = turtle.Turtle()
+        t.hideturtle()
+        t.penup()
+        state_data = data[data.state == answer_state]
+        t.goto(int(state_data.x), int(state_data.y))
+        t.write(answer_state)
 
 screen.exitonclick()
